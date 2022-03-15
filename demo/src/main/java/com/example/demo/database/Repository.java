44e0bbef40640ae068770754da0077c35afe3194
Ahmed Atya <64374947+AhmedAtya74@users.noreply.github.com>
2022-03-15 15:28:34 +0200

    Update Repository.java
    Code Review by Ahmed Atya

--- demo/src/main/java/com/example/demo/database/Repository.java
+++ demo/src/main/java/com/example/demo/database/Repository.java
@@ -1,16 +1,18 @@
 package com.example.demo.database;
 
+import java.sql.SQLException;
+
 import com.example.demo.model.Template;
 
 public interface Repository {
 
-	public boolean createNotificationTemplate(Template template);
+	public boolean createNotificationTemplate(Template template) throws SQLException;
 
-	public Template readNotificationTemplate(int templateId);
+	public Template readNotificationTemplate(int templateId) throws SQLException;
 
-	public boolean updateNotificationTemplate(int templateId, Template newTemplate);
+	public boolean updateNotificationTemplate(int templateId, Template newTemplate) throws SQLException;
 
-	public boolean deleteNotificationTemplate(int templateId);
+	public boolean deleteNotificationTemplate(int templateId) throws SQLException;
 
-	public boolean sendNotification(Template notification);
+	public boolean sendNotification(Template notification) throws SQLException;
 }
