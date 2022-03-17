d861eec139e87c7d1b3244e14d30ff39c07c6407
Ahmed Atya <64374947+AhmedAtya74@users.noreply.github.com>
2022-03-17 08:20:39 +0200

    Create SMS.java
    Code Review By Ahmed Atya

---
+++ demo/src/main/java/com/example/demo/notificationQueueApplication/SMS.java
@@ -0,0 +1,33 @@
+package NotificationQueueApplication;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.mysql.jdbc.Statement;
+
+public class SMS implements Isend {
+
+	@Override
+	public Boolean Send(int id, ArrayList<String> placeHolders) throws SQLException {
+
+		Connection connection = DB.connectToDatabase();
+		Statement statement = (Statement) connection.createStatement();
+		String selectQuery = "SELECT * FROM notificationtemplate WHERE id = " + id;
+		ResultSet result = statement.executeQuery(selectQuery);
+		if (!result.next()) {
+			return false;
+		}
+		int rand = (int) (Math.random() % 2);
+		String body = PreparationToSend(result.getString("body"), placeHolders);
+		String insertQuery = "INSERT INTO SMS values(" + "\"" + body + "\"" + ",\"" + result.getString("subject")
+				+ "\",\"" + id + "\",\"" + result.getString("language") + "\"," + "\""
+				+ placeHolders.get(placeHolders.size() - 1) + "\"," + "\"" + placeHolders.get(0) + "\"," + rand + ')';
+
+		statement.executeUpdate(insertQuery);
+
+		return rand > 0;
+	}
+
+}
